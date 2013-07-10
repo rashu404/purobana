2c9cf9f665bea74650ede2e0731414cc6f2d5724
まさ <teacupmasa@yahoo.co.jp>
2013-07-10 17:08:05 +0900

    細部の調整

--- src/com/webprog/PhysxWorldActivity.java
+++ src/com/webprog/PhysxWorldActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class MainActivity extends Activity {
+public class PhysxWorldActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
